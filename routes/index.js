3044290b7c4ad32128877ebdef49bec929fc96c4
Iván Sánchez <mazyvan@hotmail.com>
2017-07-12 01:33:42 -0500

    bien papa

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+  let sentence = req.query.sentence
+  let words = []
+  if (sentence) words = sentence.split(' ')
+  res.render('index', { 
+    sentence: req.query.sentence,
+    words: words
+  })
+})
 
 module.exports = router;
